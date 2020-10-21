c5f49edae39728553db901eadc3386ce5e73af40
MAC <mac@MACdeiMac.local>
2020-10-21 16:38:51 +0800

    init

---
+++ TZImagePickerController/TZImagePickerController/TZPhotoPickerController.h
@@ -0,0 +1,27 @@
+//
+//  TZPhotoPickerController.h
+//  TZImagePickerController
+//
+//  Created by 谭真 on 15/12/24.
+//  Copyright © 2015年 谭真. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TZAlbumModel;
+@interface TZPhotoPickerController : UIViewController
+
+@property (nonatomic, assign) BOOL isFirstAppear;
+@property (nonatomic, assign) NSInteger columnNumber;
+///是否需要30M控制  yes 需要  no  不需要
+@property (nonatomic, assign) BOOL  isNeedSizeControl;
+
+@property (nonatomic, assign) float  videoMaxDuration;
+
+@property (nonatomic, strong) TZAlbumModel *model;
+@end
+
+
+@interface TZCollectionView : UICollectionView
+
+@end
